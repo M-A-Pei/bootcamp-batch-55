9742d67badbd8b355c0a19cdc85a5b15acf8bd41
Muhammad Abdullah Syafii <syafii2006@gmail.com>
2024-06-01 09:59:39 +0700

    day 5

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,5 +1,39 @@
 let projectArray = []
 
+function getDistance(postedAt){
+  const timeNow = new Date().getTime()
+  const distance = timeNow - postedAt
+
+  const seconds = Math.floor(distance / 1000)
+  const minute = Math.floor(seconds / 60)
+  const hour = Math.floor(minute / 60)
+  const days = Math.floor(hour / 24)
+  const weeks = Math.floor(days / 7)
+  const months = Math.floor(weeks / 30)
+
+  console.log(seconds);
+  console.log(minute);
+  console.log(hour);
+  console.log(days);
+  console.log(weeks);
+  console.log(months);
+
+  if (months > 0){
+    return `${months} months ago`
+  }else if(weeks > 0){
+    return `${weeks} weeks ago`
+  }else if(days > 0){
+    return `${days} days ago`
+  }else if(hour > 0){
+    return `${hour} hours ago`
+  }else if(minute > 0){
+    return `${minute} minutes ago`
+  }else if(seconds > 0){
+    return `${seconds} seconds ago`
+  }
+
+}
+
 function handleUpload(){
     const title = document.getElementById('title').value
     const start = document.getElementById('start').value
@@ -11,7 +45,31 @@ function handleUpload(){
     const php = document.getElementById('php').checked
     let image = document.getElementById('image').files
 
-    //alert(title + start + end + description + javascript + " " + python + " " + java + " " + php)
+    let errorMsg = "you cant leave these data empty:"
+
+    if(title == ""){
+      errorMsg += "\n -project name"
+    }
+    if(start == ""){
+      errorMsg += "\n -start date"
+    }
+    if(end == ""){
+      errorMsg += "\n -end date"
+    }
+    if(description == ""){
+      errorMsg += "\n -description"
+    }
+    if(image.length == 0){
+      errorMsg += "\n -upload image"
+    }
+
+    errorMsg += "\n please fill them in"
+    
+    if(title == "" || start == ""|| end == ""|| description == ""|| image == ""){
+      alert(errorMsg)
+      return
+      
+    }
 
     image = URL.createObjectURL(image[0])
 
@@ -26,7 +84,7 @@ function handleUpload(){
 }
 
 function renderProject() {  
-    document.getElementById("content-container").innerHTML="";
+    document.getElementById("content-container").innerHTML="<h1>List of projects:</h1>";
 
     for(let i = 0; i < projectArray.length; i++){
         document.getElementById("content-container").innerHTML+=`
@@ -35,7 +93,7 @@ function renderProject() {
 
         <div class="text-container">
           <h1>${projectArray[i].title}</h1>
-          <i>${projectArray[i].description}</i>
+          <i class="desc">${projectArray[i].description}</i>
           <div class="icon-container">
             ${projectArray[i].java == true ? `<i class="fa-brands fa-java fa-lg"></i>` : ""}
             ${projectArray[i].javascript == true ? `<i class="fa-brands fa-js fa-lg"></i>` : ""}
@@ -43,8 +101,7 @@ function renderProject() {
             ${projectArray[i].php == true ? `<i class="fa-brands fa-php fa-lg"></i>` : ""}
           </div>
 
-          <span>posted at ${projectArray[i].postedAt}</span>
-          <span>ends in 3 days</span>
+          <span>${getDistance(projectArray[i].postedAt)}</span>
 
           <div class="btn-div">
             <button class="edit-btn">edit</button>
